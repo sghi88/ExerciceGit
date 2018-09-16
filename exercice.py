b1877a53ad547cf2a97c08333034534d8866ac12
sghi88 <40232706+sghi88@users.noreply.github.com>
2018-09-16 19:47:06 +0200

    plot data

---
+++ exercice.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 16 18:59:33 2018
+
+@author: vittorio
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x,y=np.loadtxt('data.txt',delimiter=None, unpack=True, skiprows=1)
+plt.plot(x,y)
+plt.show()
+
